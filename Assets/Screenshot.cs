52810d8d18f0c7e2bea9b7602f304debd6dc6f73
srikeerthis <srikeerthisrinivasan@gmail.com>
2020-03-22 10:32:49 +0530

    Added assets folder

---
+++ Assets/Screenshot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    // Capture frames as a screenshot sequence. Images are
+    // stored as PNG files in a folder 
+    // If the folder exists we will append numbers to create an empty folder.
+    string folder = "ScreenshotFolder";
+    int frameRate = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the playback framerate (real time will not relate to game time after this).
+        Time.captureFramerate = frameRate;
+        // Create the folder
+        System.IO.Directory.CreateDirectory(folder);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //path where image is stored                 
+        string name = string.Format("{0}/{1:D04} shot.png", folder, Time.frameCount );
+        //screenshot is taken
+        ScreenCapture.CaptureScreenshot(name);   
+    }
+}
